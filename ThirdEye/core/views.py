c83e284f8fb215982ce668f8890d5a2221b8ddaa
Vinícius Andrade Lopes <vinicius.anl@outlook.com>
2019-10-01 20:38:20 -0300

    Testes face detection OpenCV

--- ThirdEye/core/views.py
+++ ThirdEye/core/views.py
@@ -6,5 +6,8 @@ def homepage_view(request):
 def index_view(request):
     return render(request, 'core/description.html')
 
+def teste_web_cam(request):
+    return render(request, 'facerec_from_webcam_faster.py', {})
+
 # def base_pyview(request):
 #     return render(request, 'core/index.html')
